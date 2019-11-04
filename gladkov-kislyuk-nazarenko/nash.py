f32338ff7e8d0ad67b931ffe68b80f97136d46cb
Lev <lev199@list.ru>
2019-11-04 14:33:02 +0300

    task1
    last update

--- gladkov-kislyuk-nazarenko/nash.py
+++ gladkov-kislyuk-nazarenko/nash.py
@@ -3,18 +3,11 @@ import matplotlib.pyplot as plt
 
 
 
+def print_matrix(matrix):
+    max_len = max([len(str(e)) for r in matrix for e in r])
+    for row in matrix:
+        print(*list(map('{{:>{length}}}'.format(length=max_len).format, row)))
 
-#print("Введите количество строк:")
-#a = int(input())
-
-
-
-
-#matrix = np.random.randint(-10,10,size=(a,a))
-#matrix = [[4,2,2],[2,5,0],[0,2,5]]
-#matrix = [[1,1],[-2,3],[7,-4]]
-matrix = [[4,0,6,2,2,1],[3,8,4,10,4,4],[1,2,6,5,0,0],[6,6,4,4,10,3],[10,4,6,4,0,9],[10,7,0,7,9,8]]
-print(matrix)
 
 def first_simplex_table(i, j, A):
     matrix = np.ones((i+2,j+3)) #создание единичной матрицы 
@@ -26,7 +19,7 @@ def first_simplex_table(i, j, A):
     matrix[i+1,1] = 0 #свободный член функции цели
     matrix[i+1,2:j+2] *= -1 #коэффициенты целевой функции
     matrix[:,j+2] = 0 #обнуляем столбец для вспомогательных коэффициентов 
-    print(matrix)
+##    print(matrix)
     return matrix
 
 
@@ -36,13 +29,13 @@ def simplex_find(i, j, A):
     for n in range (0,j):
         if a[n] > 0: a[n] = 0 
     a = abs(a)        
-    print("Max",a)    
+##    print("Max",a)    
     b = np.where(a == np.amax(a))[0] #массив индексов макс.элементов инд. стр.
     if len(b) > 1: #выбираем индекс ведущего столбца:
         lead_column = b[len(b)-1]+2 #индекс ведущего столбца: +2 тк перед ним два служебных столбца
     else:
         lead_column = b[0]+2
-    print("Ведущий столбец: X" , int(A[0, lead_column])) 
+##    print("Ведущий столбец: X" , int(A[0, lead_column])) 
 
     c = np.zeros(i)
     
@@ -52,13 +45,13 @@ def simplex_find(i, j, A):
        else: 
             c[n] = A[n+1,1] / A[n+1,lead_column]
        
-    print("Min",c) #печатаем отношение свободных членов к ведущему столбцу
+##    print("Min",c) #печатаем отношение свободных членов к ведущему столбцу
     
     d = np.where(c == np.amin(c))[0] #массив индексов мин.элементов отношения
     if abs(c[d[0]]) == np.inf:
-        raise Exception("Всё очень плохо")
+        raise Exception("Пространство допустимых решений неограниченно. Решения не существует.")
     lead_line = d[0]+1 #индекс ведущей строки: +1 тк нумерация свободных переменных с 1
-    print("Ведущая строка: X" , int(A[lead_line, 0]) )
+##    print("Ведущая строка: X" , int(A[lead_line, 0]) )
     
     return lead_column, lead_line
 
@@ -68,14 +61,14 @@ def next_simplex_table(i, j, A):
     lead_column = lead_elements[0]
     lead_line = lead_elements[1]
     lead_element = A[lead_line, lead_column]
-    print(lead_elements)
+##    print(lead_elements)
     
     A[lead_line, 0], A[0, lead_column] = A[0, lead_column], A[lead_line, 0] # ведущие переменные меняем местами
     
     for n in range(1,i+2):
             if n != lead_line: A[n, j+2] = -1 * A[n,lead_column] #отсутсвие у ведущей строки вспомогательного коэффициента 
                 
-    print("Вспомогательные коэффициенты: ", A[:,j+2])
+##    print("Вспомогательные коэффициенты: ", A[:,j+2])
                 
     for n in range(1,j+2): #заполняем новый базисный элемент
             if n == lead_column: 
@@ -83,7 +76,7 @@ def next_simplex_table(i, j, A):
             else:
                 A[lead_line, n] /=  lead_element
                
-    print("Новый базисный элемент: ", A[lead_line])
+##    print("Новый базисный элемент: ", A[lead_line])
     
     for n in range(1,i+2): #внешний цикл по строкам
             if n != lead_line: #заполняем оставшиеся базисные элементы
@@ -92,7 +85,7 @@ def next_simplex_table(i, j, A):
                         A[n,m] = A[lead_line,m]*A[n, j+2] #+0 тк в предыдущей таблице не было столбца у базисного элемента
                     else:
                         A[n,m] = A[lead_line,m]*A[n, j+2]+A[n,m]
-    print(A)
+##    print(A)
 
 
 
@@ -105,7 +98,7 @@ def optimality_criterion(i, j, A):
 
 
 
-def nash_equilibrium(A):
+def nash_equilibrium(A,gr):
 
     a, b = (np.shape(A)) #размер матрицы: кол-во строк, кол-во столбцов 
 
@@ -113,7 +106,7 @@ def nash_equilibrium(A):
 
     count = 0 #стетчик итераций симплекс-метода
     while not optimality_criterion(a, b, B): #проверка выполнения критерия оптимальности
-        print("Итерация №", count)
+##        print("Итерация №", count)
         next_simplex_table(a, b, B) #построение следующей симплексной таблицы
         count += 1
     
@@ -124,37 +117,31 @@ def nash_equilibrium(A):
             solution_to_primal[int(B[n+1,0])-1] = B[n+1,1]
     for n in range(b): #выписываем решение двойственной задачи
         if a+b >= B[0,n+2] > b:
-            solution_to_dual[int(B[0,n+2])-a-1] = B[a+1,n+2]
+            solution_to_dual[int(B[0,n+2])-b-1] = B[a+1,n+2]
 
     price = 1/np.sum(solution_to_primal) #цена игры
+    strategy1=price*solution_to_dual
+    strategy2=price*solution_to_primal
 
-    print("Значение игры:", price)
-    print("Оптимальная стратегия первого игрока:", price*solution_to_dual)
-    print("Оптимальная стратегия второго игрока:", price*solution_to_primal)
+    #print("Значение игры:", price)
+    #print("Оптимальная стратегия первого игрока:", strategy1)
+    #print("Оптимальная стратегия второго игрока:", strategy2)
     
-    x = np.arange(1, a+1)
-    y = price*solution_to_dual
-    fig, ax = plt.subplots()
+    if (gr):
+        x = np.arange(1, a+1)
+        y = price*solution_to_dual
+        fig, ax = plt.subplots()
 
-    ax.scatter(x, y)
+        ax.scatter(x, y)
 
-    plt.show()
+        plt.show()
     
-    x = np.arange(1, b+1)
-    y = price*solution_to_primal
-    fig, ax = plt.subplots()
+        x = np.arange(1, b+1)
+        y = price*solution_to_primal
+        fig, ax = plt.subplots()
 
-    ax.scatter(x, y)
+        ax.scatter(x, y)
 
-    plt.show()
+        plt.show()
 
-    
-
-nash_equilibrium(matrix)
-
-
-
-
-
-
-
+    return price, strategy1, strategy2
